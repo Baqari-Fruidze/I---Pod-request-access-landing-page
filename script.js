20a4840f3d8d884b703e30f25a8f8c071bbd0d4e
Baqari-Fruidze <baqarifruidze@gmail.com>
2024-03-05 22:26:47 +0400

    final

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("hello");
+let input = document.querySelector(".input");
+let button = document.querySelector(".btn");
+let erorMesage = document.querySelector(".validation-span");
+const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+// const inputTest = regex.test(input.textContent);
+button.addEventListener("click", () => {
+  const inputTest = regex.test(input.value);
+  if (!inputTest) {
+    erorMesage.style.display = "block";
+  } else {
+    erorMesage.style.display = "none";
+  }
+});
